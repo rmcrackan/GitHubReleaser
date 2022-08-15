083b32bc177637933e519338b8250c08f894a861
Robert McRackan <rmcrackan@gmail.com>
2022-08-14 20:00:23 -0400

    include OS-specific ConfigApp

--- GitHubReleaser/LibationSettings.cs
+++ GitHubReleaser/LibationSettings.cs
@@ -9,78 +9,119 @@ using Dinah.Core.WindowsDesktop.Processes;
 
 namespace GitHubReleaser
 {
-	public abstract class LibationExe : Enumeration<LibationExe>
-	{
-		public static LibationExe Classic { get; } = new ClassicExe();
-		public static LibationExe Chardonnay { get; } = new ChardonnayExe();
+	public abstract class Project : Enumeration<Project>
+    {
+        public static Project LibationCli { get; } = new LibationCli_Project();
+        public static Project HangoverWinForms { get; } = new HangoverWinForms_Project();
+        public static Project HangoverAvalonia { get; } = new HangoverAvalonia_Project();
 
+        public static Project LibationExe_Classic => LibationExe.Classic;
+        public static Project LibationExe_Chardonnay => LibationExe.Chardonnay;
+
+        public string CsProj { get; }
 		public string Name { get; }
-		public string CsProj { get; }
+        protected virtual string SubDir => "";
+		protected virtual string ProjDir => CsProj;
+		public string Dir => $@"{SubDir}\{ProjDir}".Trim('\\');
 
-		private static int i = 0;
-		public LibationExe(string name, string csProj) : base(i++, name)
-		{
-			Name = name;
-			CsProj = csProj;
-		}
+        private static int i = 0;
+		public Project(string csProj) : this(csProj, csProj) { }
+        protected Project(string csProj, string name) : base(i++, csProj)
+        {
+            CsProj = ArgumentValidator.EnsureNotNullOrEmpty(csProj, nameof(csProj));
+			Name = string.IsNullOrWhiteSpace(name) ? csProj : name;
+        }
 
-		private class ClassicExe : LibationExe { public ClassicExe() : base("classic", "LibationWinForms") { } }
-		private class ChardonnayExe : LibationExe { public ChardonnayExe() : base("chardonnay", "LibationAvalonia") { } }
+        private class LibationCli_Project : Project { public LibationCli_Project() : base(
+            csProj: "LibationCli") { } }
+        private class HangoverWinForms_Project : Project { public HangoverWinForms_Project() : base(
+            csProj: "HangoverWinForms") { } }
+        private class HangoverAvalonia_Project : Project { public HangoverAvalonia_Project() : base(
+            csProj: "HangoverAvalonia") { } }
+
+		public override string ToString()
+			=> $"{GetType()}"
+			+ $"|{nameof(CsProj)}:{CsProj}"
+			+ $"|{nameof(Name)}:{Name}"
+			+ $"|{nameof(Dir)}:{Dir}";
 	}
 
-	enum Project { LibationCli, HangoverWinForms, HangoverAvalonia }
+	public abstract class LibationExe : Project
+    {
+        public static LibationExe Classic { get; } = new ClassicExe();
+        public static LibationExe Chardonnay { get; } = new ChardonnayExe();
 
-	public abstract class OperatingSystems : Enumeration<OperatingSystems>
+		public LibationExe(string csProj, string name) : base(csProj: csProj, name: name) { }
+
+		private class ClassicExe : LibationExe { public ClassicExe() : base(
+			csProj: "LibationWinForms",
+			name: "classic") { } }
+        private class ChardonnayExe : LibationExe { public ChardonnayExe() : base(
+            csProj: "LibationAvalonia",
+            name: "chardonnay") { }
+        }
+    }
+
+	public abstract class ConfigApp : Project
+    {
+        public static ConfigApp WindowsConfigApp { get; } = new Windows_ConfigApp();
+        public static ConfigApp LinuxConfigApp { get; } = new Linux_ConfigApp();
+        public static ConfigApp MacOSConfigApp { get; } = new MacOS_ConfigApp();
+
+        protected override string SubDir => @"LoadByOS";
+		public ConfigApp(OperatingSystem os) : base(os.Name + "ConfigApp") { }
+
+        private class Windows_ConfigApp : ConfigApp { public Windows_ConfigApp() : base(OperatingSystem.Windows) { } }
+        private class Linux_ConfigApp : ConfigApp { public Linux_ConfigApp() : base(OperatingSystem.Linux) { } }
+        private class MacOS_ConfigApp : ConfigApp { public MacOS_ConfigApp() : base(OperatingSystem.MacOS) { } }
+	}
+
+	public abstract class OperatingSystem : Enumeration<OperatingSystem>
 	{
-		public static OperatingSystems Windows { get; } = new WindowsOperatingSystem();
-		public static OperatingSystems Linux { get; } = new LinuxOperatingSystem();
-		public static OperatingSystems MacOS { get; } = new MacOperatingSystem();
+		public static OperatingSystem Windows { get; } = new WindowsOperatingSystem();
+		public static OperatingSystem Linux { get; } = new LinuxOperatingSystem();
+		public static OperatingSystem MacOS { get; } = new MacOSOperatingSystem();
 
 		public string Name { get; }
 		public string Abbrev { get; }
 
 		private static int i = 0;
-		public OperatingSystems(string name, string abbrev) : base(i++, name)
+		public OperatingSystem(string name, string abbrev) : base(i++, name)
 		{
 			Name = name;
 			Abbrev = abbrev;
 		}
 
-		private class WindowsOperatingSystem : OperatingSystems { public WindowsOperatingSystem() : base("Windows", "win") { } }
-		private class LinuxOperatingSystem : OperatingSystems { public LinuxOperatingSystem() : base("Linux", "linux") { } }
-		private class MacOperatingSystem : OperatingSystems { public MacOperatingSystem() : base("MacOS", "macos") { } }
-	}
+		private class WindowsOperatingSystem : OperatingSystem { public WindowsOperatingSystem() : base(
+			name: "Windows",
+			abbrev: "win") { } }
+		private class LinuxOperatingSystem : OperatingSystem { public LinuxOperatingSystem() : base(
+			name: "Linux",
+			abbrev: "linux") { } }
+		private class MacOSOperatingSystem : OperatingSystem { public MacOSOperatingSystem() : base(
+			name: "MacOS",
+			abbrev: "macos") { } }
+
+        public override string ToString() => $"{nameof(OperatingSystem)}:{Name}";
+    }
 
 	class Release
 	{
-		public OperatingSystems OS { get; init; }
-		public LibationExe LibationExe { get; init; }
+		public OperatingSystem OS { get; init; }
 		public string BuildPrefix { get; init; } = "";
 
-		// order matters
-		public List<Project> OtherProjects { get; init; } = new List<Project>();
-
-		public List<string> Projects
-		{
-			get
-			{
-				var list = new List<string>();
-
-				if (LibationExe is not null)
-					list.Add(LibationExe.CsProj.ToString());
-
-				list.AddRange(OtherProjects.Select(p => $"{p}"));
-				return list;
-			}
-		}
+        /// <summary>order matters. First project is main/LibationExe</summary>
+        public List<Project> Projects { get; init; } = new();
+        /// <summary>LibationExe</summary>
+        public Project MainExe => Projects.FirstOrDefault();
 	}
 	static class ReleaseExtensions
     {
 		public const string BIN_PUBLISH = @"bin\Publish";
 
-		public static string GetPublishDirRelative(this Release release) => Path.Combine(BIN_PUBLISH, $@"{release.OS.Abbrev}-{release.LibationExe.Name}");
+		public static string GetPublishDirRelative(this Release release) => Path.Combine(BIN_PUBLISH, $@"{release.OS.Abbrev}-{release.MainExe.Name}");
 
-		public static string GetZipDir(this Release release) => $@"{release.BuildPrefix}Libation.#.#.#-{release.OS.Abbrev}-{release.LibationExe.Name}";
+		public static string GetZipDir(this Release release) => $@"{release.BuildPrefix}Libation.#.#.#-{release.OS.Abbrev}-{release.MainExe.Name}";
 	}
 
     internal class LibationSettings : ProjectSettings
@@ -88,35 +129,29 @@ namespace GitHubReleaser
 		// order matters. upload classic FIRST
 		List<Release> releases { get; } = new()
 		{
-			// order matters. build hangover LAST
+			// order matters:
+			// * main exe must be FIRST
+			// * build hangover LAST
 			new()
 			{
-				OS = OperatingSystems.Windows,
-				LibationExe = LibationExe.Classic,
-
+				OS = OperatingSystem.Windows,
 				BuildPrefix = "Classic-",
-				OtherProjects = new(){ Project.LibationCli, Project.HangoverWinForms },
+				Projects = new(){ LibationExe.Classic, ConfigApp.WindowsConfigApp, Project.LibationCli, Project.HangoverWinForms },
 			},
 			new()
 			{
-				OS = OperatingSystems.Windows,
-				LibationExe = LibationExe.Chardonnay,
-
-				OtherProjects = new(){ Project.LibationCli, Project.HangoverAvalonia }
+				OS = OperatingSystem.Windows,
+				Projects = new(){ LibationExe.Chardonnay, ConfigApp.WindowsConfigApp, Project.LibationCli, Project.HangoverAvalonia }
 			},
 			new()
 			{
-				OS = OperatingSystems.Linux,
-				LibationExe = LibationExe.Chardonnay,
-
-				OtherProjects = new(){ Project.LibationCli, Project.HangoverAvalonia }
+				OS = OperatingSystem.Linux,
+                Projects = new(){ LibationExe.Chardonnay, ConfigApp.LinuxConfigApp, Project.LibationCli, Project.HangoverAvalonia }
 			},
 			new()
 			{
-				OS = OperatingSystems.MacOS,
-				LibationExe = LibationExe.Chardonnay,
-
-				OtherProjects = new(){ Project.LibationCli, Project.HangoverAvalonia }
+				OS = OperatingSystem.MacOS,
+                Projects = new(){ LibationExe.Chardonnay, ConfigApp.MacOSConfigApp, Project.LibationCli, Project.HangoverAvalonia }
 			}
 		};
 
@@ -142,7 +177,7 @@ I intend to keep Libation free and open source, but if you want to [leave a tip]
 
 			foreach (var r in releases)
 			{
-				logMe($@"{r.OS}|{r.LibationExe}");
+				logMe($@"{r.OS}|{r.MainExe}");
 
 				await runDotNetAsync("clean -c Release");
 
@@ -150,8 +185,8 @@ I intend to keep Libation free and open source, but if you want to [leave a tip]
 				{
 					logMe($"* project:{project}");
 
-					var csproj = $@"{project}\{project}.csproj";
-					var pubxml = $@"{project}\Properties\PublishProfiles\{r.OS.Name}Profile.pubxml";
+					var csproj = $@"{project.Dir}\{project.CsProj}.csproj";
+					var pubxml = $@"{project.Dir}\Properties\PublishProfiles\{r.OS.Name}Profile.pubxml";
 
 					await runDotNetAsync($"publish -c Release -o {r.GetPublishDirRelative()} {csproj} -p:PublishProfile={pubxml}");
 				}
